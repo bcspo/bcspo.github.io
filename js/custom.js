996d15d24b217b99983b739cd0d1a7381bd50aea
bcspo <169110098+bcspo@users.noreply.github.com>
2024-11-14 09:33:40 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,30 @@ $(function () {
     const tl = gsap.timeline();
 
     tl
-        .from('.intro h2', { x: -1000, opacity: 0, delay: 1 })
-        .from('.intro p', { x: -1000, opacity: 0 })
-        .from('.intro .star', { y: 200, opacity: 0, rotate: 720 })
+        .from('.intro .tit .top', {
+            x: -200,
+            opacity: 0,
+            duration: 1.2,
+        })
+        .from('.intro .tit .middle', {
+            x: -200,
+            opacity: 0,
+            duration: 1.2,
+        })
+        .from('.intro .tit .bottom', {
+            x: -200,
+            opacity: 0,
+            duration: 1.2,
+        })
+        .from('.intro .stars', {
+            opacity: 0,
+            duration: 1.2,
+        })
+
 
     $('#content').fullpage({
-        anchors: ['intro', 'pf01', 'pf02', 'pf03', 'profile'],
+        anchors: ['intro', 'pf01', 'pf02', 'pf03', 'pf04', 'pf05', 'training', 'profile'],
+        responsiveWidth: 768,
         onLeave: function (idx, nidx, dir) {
             console.log(idx, nidx, dir);
             $('#header .gnb li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
@@ -18,12 +36,14 @@ $(function () {
             if (nidx == 1) {
                 tl.restart();
             }
+
+            if (nidx == 1) {
+                $('#header .gnb').removeClass('on')
+            } else {
+                $('#header .gnb').addClass('on')
+            }
         }
     });
-
-    // $('#header .gnb li a').on('click', function () {
-    //     $(this).parent().addClass('on').siblings().removeClass('on')
-    // });
 
 
     $('#header .menu').on('click', function () {
@@ -40,6 +60,15 @@ $(function () {
 
     $('#header .cover').on('wheel', function (e) {
         e.stopPropagation();
+    });
+
+    const desc = new Swiper(".profile .desc", {
+        direction: "vertical",
+        loop: true,
+        autoplay: {
+            delay: 1700,
+            disableOnInteraction: false
+        },
     });
 
 });
